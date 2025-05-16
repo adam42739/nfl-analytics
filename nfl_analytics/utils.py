fb87e75f66d810c9bd092da5ceb76153a67950b0
adam42739 <adamclynch03@gmail.com>
2025-05-16 15:41:41 -0400

    filter data function

--- nfl_analytics/utils.py
+++ nfl_analytics/utils.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 class NflWeek:
     """
     A class to represent a week in the NFL season.
@@ -12,3 +15,39 @@ class NflWeek:
 
     def __repr__(self):
         return f"NflWeek(year={self.season}, week={self.week})"
+
+
+def filter_data(
+    df: pd.DataFrame,
+    after_week: NflWeek = NflWeek(1900, 1),
+    before_week: NflWeek = NflWeek(2100, 1),
+    season_col: str = "season",
+    week_col: str = "week",
+) -> pd.DataFrame:
+    """
+    Filter the DataFrame based on the given season and week range.
+
+    Parameters
+    ----------
+        df : pd.DataFrame
+            The DataFrame to filter.
+        after_week : NflWeek
+            The start week to filter from. Leave blank for no lower bound.
+        before_week : NflWeek
+            The end week to filter to. Leave blank for no upper bound.
+        season_col : str
+            The name of the column containing the season information. Default is "season".
+        week_col : str
+            The name of the column containing the week information. Default is "week".
+
+    Returns
+    -------
+        pd.DataFrame
+            The filtered DataFrame. Beware returned DataFrame is a slice.
+    """
+    return df[
+        (df[season_col] >= after_week.season)
+        & (df[season_col] <= before_week.season)
+        & (df[week_col] >= after_week.week)
+        & (df[week_col] <= before_week.week)
+    ]
